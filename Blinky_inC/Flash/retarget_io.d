24a1b2c4532a402d3a54da9d9c5479057d7bcac3
vikgomat <vikgomat@yahoo.com>
2024-02-21 08:02:37 -0500

    Blinky_inC

---
+++ Blinky_inC/Flash/retarget_io.d
@@ -0,0 +1,9 @@
+./flash/retarget_io.o: \
+  C:\Users\vikgo\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.7.2\Source\retarget_io.c \
+  C:\Keil_v5\ARM\ARMCLANG\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\assert.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\rt_sys.h \
+  RTE\_STM32L476_Flash\RTE_Components.h
---
+++ Blinky_inC/RTE/_STM32L476_Flash/RTE_Components.h
@@ -0,0 +1,26 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'STM32L476 Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+/* Keil::Compiler&ARM Compiler:I/O:STDOUT&User@1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+/* Keil::Device:Startup@1.3.1 */
+#define RTE_DEVICE_STARTUP_STM32L4XX    /* Device Startup for STM32L4 */
+
+
+#endif /* RTE_COMPONENTS_H */
